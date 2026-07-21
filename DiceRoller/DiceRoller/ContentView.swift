d4332f41152adda30ab722bf3dd8176b3d352d02
Emir Keles <burhanemirkeles@outlook.com>
2026-07-21 16:49:38 +0300

    create DiceRoller project

---
+++ DiceRoller/DiceRoller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiceRoller
+//
+//  Created by Infonal on 21.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
