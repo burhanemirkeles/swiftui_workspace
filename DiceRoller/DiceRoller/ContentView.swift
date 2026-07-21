f993d651c140b3961b4e6364dfd6853a1fa89201
Emir Keles <burhanemirkeles@outlook.com>
2026-07-21 17:45:13 +0300

    add diceRoller app

--- DiceRoller/DiceRoller/ContentView.swift
+++ DiceRoller/DiceRoller/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numberOfDices: Int = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dice Roller")
+                .font(.largeTitle.lowercaseSmallCaps())
+                .foregroundStyle(.white)
+            
+            HStack {
+                ForEach(1...numberOfDices, id: \.description) { _ in
+                    DiceView()
+                }
+            }
+            HStack {
+                Button("Remove Dice", systemImage: "minus.circle.fill") {
+                    withAnimation {
+                        numberOfDices -= 1
+                    }
+                }
+                .disabled(numberOfDices == 1)
+                Button("Add Dice", systemImage: "plus.circle.fill") {
+                    withAnimation {
+                        numberOfDices += 1
+                    }
+                }
+                .disabled(numberOfDices == 5)
+            }
+            .padding()
+            .labelStyle(.iconOnly)
+            .font(.title)
+            
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.appBackground)
+        .tint(.white)
     }
 }
 
