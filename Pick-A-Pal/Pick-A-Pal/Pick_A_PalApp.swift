718d45c04c268700fdc613a0b822baf98d6e047d
Emir Keles <burhanemirkeles@outlook.com>
2026-07-22 11:09:55 +0300

    add Pick-a-Pal base app

---
+++ Pick-A-Pal/Pick-A-Pal/Pick_A_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_A_PalApp.swift
+//  Pick-A-Pal
+//
+//  Created by Infonal on 22.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct Pick_A_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
