a741cd317de5b267b8776531a2b8d3c08eecf1d0
Emir Keles <burhanemirkeles@outlook.com>
2026-07-22 17:13:36 +0300

    add testing

---
+++ ScoreKeeper/ScoreKeeper/Scoreboard.swift
@@ -0,0 +1,24 @@
+//
+//  Scoreboard.swift
+//  ScoreKeeper
+//
+//  Created by Emir Keles on 22.07.2026.
+//
+
+import Foundation
+
+struct Scoreboard {
+    var players: [Player] = [
+        Player(name: "Elisha", score: 0),
+        Player(name: "Andre", score: 0),
+        Player(name: "Jasmine", score: 0),
+    ]
+    
+    var state = GameState.setup
+    
+    mutating func resetScores(to newValue: Int) {
+        for index in 0..<players.count {
+            players[index].score = newValue
+        }
+    }
+}
